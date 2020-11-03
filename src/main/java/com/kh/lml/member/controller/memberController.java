537c5c96f7f562702594ed9590ecd4d59a8f1f11
keunji <dmswl2177@gmail.com>
2020-11-03 15:49:41 +0900

    d

--- src/main/java/com/kh/lml/member/controller/memberController.java
+++ src/main/java/com/kh/lml/member/controller/memberController.java
@@ -17,6 +17,22 @@ public class memberController {
 	public String setting() {
 		return "settings/lml_Settings";
 	}
+	@RequestMapping("Settings2.do")
+	   public String setting2() {
+	      return "settings/lml_Settings_2";
+	   }
+	   @RequestMapping("Settings3.do")
+	   public String setting3() {
+	      return "settings/lml_Settings_3";
+	   }
+	   @RequestMapping("Settings4.do")
+	   public String setting4() {
+	      return "settings/lml_Settings_4";
+	   }
+	   @RequestMapping("Settings5.do")
+	   public String setting5() {
+	      return "settings/lml_Settings_5";
+	   }
 	@RequestMapping("Index.do")
 	public String index() {
 		return "../../index";
